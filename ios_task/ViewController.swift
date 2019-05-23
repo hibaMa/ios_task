d0d54c68a88ba1e0f67f514f5495e95e8232e7ae
Hiba Malhis <hibamalhis@hibas-macbook-air.local>
2019-05-23 17:25:34 +0300

    Initial Commit

---
+++ ios_task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_task
+//
+//  Created by Hiba Malhis on 5/23/19.
+//  Copyright © 2019 Hiba Malhis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
